e2dd37b6053a35478f367a6cd7d364e95976d1c2
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-10 13:12:46 +0530

    [Tushar Vidwans] Checking whether a prime number is Palindrome or not

---
+++ Check.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberRange
+{
+    public class Check
+    {
+        public void isPalindrome(int PrimeNum)
+        {
+
+            int rem, rev = 0, num = PrimeNum;
+
+            while (num != 0)
+            {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num = num / 10;
+            }
+
+            if(rev == PrimeNum)
+            {
+                Console.WriteLine("It is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("It is not Palindrome");
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,37 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Prime number Program");
-            
+            Check check = new Check();
+            int status = 1;
+            Console.WriteLine("Enter the range of numbers from 2 to check it is Prime or Not: ");
+            Console.Write("start: ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.Write("End: ");
+            int end = Convert.ToInt32(Console.ReadLine());
+            int num = start;
+
+            Console.WriteLine("Prime Numbers are: ");
+            while (num <= end)
+            {
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        //Console.WriteLine($"{num} is not a prime number");
+                        status = 0;
+                        break;
+                    }
+                }
+                if (status == 1)
+                {
+                    Console.Write($"Prime Number: {num} :");
+                    check.isPalindrome(num);
+                }
+
+                num++;
+                status = 1;
+            }
+
         }
     }
 }
