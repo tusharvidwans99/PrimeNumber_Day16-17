dbafc604e7d692b0b22399950fabb50eeb928b65
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-10 13:10:19 +0530

    [Tushar Vidwans] Adding Welcome message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PrimeNumberRange
+{
+
+    /// <summary>
+    /// In this program we are going to see multiple operation on prime numbers and on Collections.
+    /// </summary>
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Prime number Program");
+            
+        }
+    }
+}
